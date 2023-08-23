54aeac6d8796527a56e749c8b8a4929ea978bc21
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-08-23 21:45:44 +0900

    chore: update Whitelist testing script

--- packages/contract/test/Whitelist.test.js
+++ packages/contract/test/Whitelist.test.js
@@ -20,7 +20,7 @@ describe('Whitelist', function () {
       it('reverts', async function () {
         // テストの準備をします。
         const { whitelist, alice, bob } = await loadFixture(
-            deployWhitelistFixture,
+          deployWhitelistFixture,
         );
         // 実行＆確認をします。
         await expect(whitelist.connect(alice).addToWhitelist(bob.address)).to.be.revertedWith(
@@ -28,7 +28,7 @@ describe('Whitelist', function () {
         );
       });
     });
-    context('when user is already added', function () {
+    context('when address is already added', function () {
       it('reverts', async function () {
         const { whitelist, alice } = await loadFixture(
           deployWhitelistFixture,
@@ -38,7 +38,7 @@ describe('Whitelist', function () {
         );
       });
     });
-    context('when adding a new user address', function () {
+    context('when adding a new address', function () {
       it('emit a AddToWhitelist event', async function () {
         const { whitelist, bob } = await loadFixture(
           deployWhitelistFixture,
@@ -51,10 +51,54 @@ describe('Whitelist', function () {
   });
 
   describe('removeFromWhitelist', function () {
-      // TODO: add test
+    context('when user is not owner', function () {
+      it('reverts', async function () {
+        const { whitelist, alice, bob } = await loadFixture(
+          deployWhitelistFixture,
+        );
+        await expect(whitelist.connect(alice).removeFromWhitelist(bob.address)).to.be.revertedWith(
+          'Caller is not the owner',
+        );
+      });
+    });
+    context('when an address is not in whitelist', function () {
+      it('reverts', async function () {
+        const { whitelist, bob } = await loadFixture(
+          deployWhitelistFixture,
+        );
+        await expect(whitelist.removeFromWhitelist(bob.address)).to.be.revertedWith(
+          'Address not in whitelist',
+        );
+      });
+    });
+    context('when removing an address', function () {
+      it('emit a RemoveFromWhitelist event', async function () {
+        const { whitelist, alice } = await loadFixture(
+          deployWhitelistFixture,
+        );
+        await expect(whitelist.removeFromWhitelist(alice.address))
+          .to.emit(whitelist, 'RemoveFromWhitelist')
+          .withArgs(alice.address);
+      });
+    });
   });
 
   describe('whitelistedAddresses', function () {
-      // TODO: add test
+    context('when an address is not in whitelist', function () {
+      it('returns false', async function () {
+        const { whitelist, bob } = await loadFixture(
+          deployWhitelistFixture,
+        );
+        expect(await whitelist.whitelistedAddresses(bob.address)).to.be.false;
+      });
+    });
+    context('when an address is in whitelist', function () {
+      it('returns true', async function () {
+        const { whitelist, alice } = await loadFixture(
+          deployWhitelistFixture,
+        );
+        expect(await whitelist.whitelistedAddresses(alice.address)).to.be.true;
+      });
+    });
   });
 });
