36cd496de3b5943a5c019a33bde064698619b8e2
neila <neila@shiftbase.xyz>
2023-09-14 16:26:15 +0900

    lint fix

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,7 @@
 const path = require('path');
 
 const buildSolhintCommand = (filenames) =>
-  `solhint --max-warnings 0 ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' ')} `;
+  `solhint ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')} `;
 
 const buildLintCommand = (filenames) =>
   `eslint ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`;
